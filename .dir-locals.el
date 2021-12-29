28af540a56a284249c82c5c2c31d4029ef08de6d
Jonas Collberg <jonas.collberg@gmail.com>
2021-12-29 15:42:04 +0100

    01-GADTs: Set up Nix flake

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+((nil . ((lsp-file-watch-threshold . 20000)))
+ (haskell-mode . ((haskell-stylish-on-save . t)
+                  (haskell-mode-stylish-haskell-path . "brittany")
+                  (fill-column . 80))))
