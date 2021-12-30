c2950dae1a02ec1de67f30a4a13fda033e09556c
Jonas Collberg <jonas.collberg@gmail.com>
2021-12-30 10:38:56 +0100

    02-FlexibleInstances: Solve exercises

--- 02-FlexibleInstances/src/Exercises.hs
+++ 02-FlexibleInstances/src/Exercises.hs
@@ -6,29 +6,29 @@ class PopQuiz a
 -- :D This is a tricky one, but look out for nested concrete types!
 
 -- instance PopQuiz Bool
--- instance PopQuiz [Bool]
+-- instance PopQuiz [Bool] -- requires `FlexibleInstances`
 -- instance PopQuiz [a]
 -- instance PopQuiz (a, b)
--- instance PopQuiz [(a, b)]
+-- instance PopQuiz [(a, b)] -- requires `FlexibleInstances`
 -- instance PopQuiz (IO a)
 
 newtype RIO  r a = RIO (r -> IO a) -- Remember, this is a /new type/.
 type    RIO' r a =      r -> IO a
 
--- instance PopQuiz (RIO Int a)
+-- instance PopQuiz (RIO Int a) -- requires FlexibleInstances
 -- instance PopQuiz (RIO r a)
--- instance PopQuiz (RIO' r a)
--- instance PopQuiz (r -> IO a)
+-- instance PopQuiz (RIO' r a) -- requires FlexibleInstances
+-- instance PopQuiz (r -> IO a) -- requires FlexibleInstances
 -- instance PopQuiz (a -> b) -- We can write (a -> b) as ((->) a b).
--- instance PopQuiz (a -> b -> c)
+-- instance PopQuiz (a -> b -> c) -- requires FlexibleInstances
 -- instance PopQuiz (a, b, c)
--- instance PopQuiz (a, (b, c))
+-- instance PopQuiz (a, (b, c)) -- requires FlexibleInstances
 -- instance PopQuiz ()
--- instance PopQuiz (a, b, c, a)
+-- instance PopQuiz (a, b, c, a) -- requires FlexibleInstances
 
 data Pair  a = Pair  a  a
 type Pair' a =      (a, a)
 
--- instance PopQuiz (a, a)
+-- instance PopQuiz (a, a) -- requires FlexibleInstances
 -- instance PopQuiz (Pair a)
--- instance PopQuiz (Pair' a)
+-- instance PopQuiz (Pair' a) -- requires FlexibleInstances
