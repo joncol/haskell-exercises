358cb480685f6997ee50fa9ae0923ef6628feaf1
Jonas Collberg <jonas.collberg@gmail.com>
2022-01-01 17:54:16 +0100

    06-TypeFamilies: Set up Nix flake

---
+++ 06-TypeFamilies/flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "exercise06";
+
+  inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
+  inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils, haskellNix }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+      let
+        overlay = self: _: {
+          hsPkgs = self.haskell-nix.project' rec {
+            src = ./.;
+            compiler-nix-name = "ghc8107";
+            shell = {
+              tools = {
+                cabal = { };
+                ghcid = { };
+                haskell-language-server = { };
+                hlint = { };
+                hoogle = { };
+                stylish-haskell = { };
+              };
+
+              # Non-Haskell shell tools.
+              buildInputs = with pkgs; [ graphviz ];
+
+              withHoogle = false;
+            };
+          };
+        };
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ haskellNix.overlay overlay ];
+        };
+        flake = pkgs.hsPkgs.flake { };
+      in flake // {
+        defaultPackage = flake.packages."exercise06:exe:exercise06";
+      });
+}
